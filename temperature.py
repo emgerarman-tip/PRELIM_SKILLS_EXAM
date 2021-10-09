99ae3872b75fa3797a2f4644f2e0001ec02f93c9
emgerarman-tip <qemggerarman@tip.edu.ph>
2021-10-09 01:50:03 +0000

    CSV

--- temperature.py
+++ temperature.py
@@ -8,15 +8,17 @@ class Temperature:
         if len(values) > 1:
             raise ValueError('Only one argument')
 
+
         if celsius is not None:
             self.kelvin = celsius + 273.15
         elif fahrenheit is not None:
-            self.kelvin = (fahrenheit - 32) * 5 / 9 + 273.15
+            self.kelvin  = (fahrenheit - 32) * 5/9 + 273.15
         else:
             self.kelvin = kelvin
 
         if self.kelvin < 0:
             raise ValueError('Temperature in Kelvin cannot be negative')
 
-def __str__(self):
-    return f'Temperature = {self.kelvin} Kelvins'
+
+    def __str__(self):
+        return f'Temperature = {self.kelvin} Kelvins'
--- temperature_test.py
+++ temperature_test.py
@@ -5,12 +5,12 @@ class TestTemperature (unittest.TestCase):
 
     def test_kelvin (self):
         self.assertEqual(Temperature(2).kelvin, 2)
-        
+
     def test_celsius (self):
         celsius_test = 3 + 273.15
         temp = Temperature(celsius=3)
         self.assertEqual(temp.kelvin, 276.15)
-    
+
     def test_fahrenheit (self):
         fahrenheit_test = (5 - 32) *5/9 + 273.15
         temp = Temperature(fahrenheit=5)
